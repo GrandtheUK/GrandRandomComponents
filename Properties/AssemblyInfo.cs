aed9ad6889b7ec953e27f79786ef8eaead3be55f
Grand <grand.uk@tutanota.com>
2026-04-08 10:04:01 +0100

    Initial ValueSpatialVariableCollector Implementation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using Elements.Data;
 
-[assembly: DataModelAssembly(DataModelAssemblyType.Core)]
+[assembly: DataModelAssembly(DataModelAssemblyType.Optional)]
---
+++ ValueSpatialVariableCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Elements.Core;
+using FrooxEngine;
+using ProtoFlux.Runtimes.Execution;
+
+namespace TestPlugin;
+
+[Category("Data/Spatial/Samplers/Lists")]
+class ValueSpatialVariableCollector<T> : Component
+{
+	public readonly DriveRef<SyncFieldList<T>> ValueList;
+	public readonly Sync<string> VariableName;
+	public readonly Sync<T> DefaultTarget;
+	protected override void OnCommonUpdate()
+	{
+		base.OnCommonUpdate();
+		if (!ValueList.IsLinkValid)
+			return;
+		List<SpatialVariableResult<T>> list = Pool.BorrowList<SpatialVariableResult<T>>();
+		World.SampleSpatialVariables<T>((string)VariableName, Slot.GlobalPosition, list);
+		SyncFieldList<T> target = ValueList.Target;
+		for (int index = 0; index < list.Count; ++index)
+		{
+			if (target.Count == index)
+				target.Add(list[index].value);
+			else
+				target[index] = list[index].value;
+		}
+		while (target.Count > list.Count)
+			target.RemoveAt(target.Count - 1);
+		Pool.Return(ref list);
+	}
+}
